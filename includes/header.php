8aa58465720868ea47cbd83369e8f185e77894eb
steven opio <stevenviko@gmail.com>
2021-01-13 00:20:01 +0300

    Update links

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <title>Home :: Kazuke</title>
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
 <meta name="keywords" content="Real Estate, saloon services , construction services, Tours and travels "/>
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 		function hideURLbar(){ window.scrollTo(0,1); } </script>
